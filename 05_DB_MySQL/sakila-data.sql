1c5fac3abbdc0cacf269e6a44c203eb9d222c67b
KanSunghoon <sunghoon.k0210@gmail.com>
2025-07-07 17:45:09 +0900

    250707

--- 05_DB_MySQL/sakila-data.sql
+++ 05_DB_MySQL/sakila-data.sql
@@ -3,7 +3,6 @@
 
 
 -- Copyright (c) 2006, 2025, Oracle and/or its affiliates.
-
 -- Redistribution and use in source and binary forms, with or without
 -- modification, are permitted provided that the following conditions are
 -- met:
